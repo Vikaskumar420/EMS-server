a05e8ce18c1868727b4e8c7149f186ad45cbd019
Vikaskumar420 <vikaskumar766820@gmail.com>
2026-02-12 23:40:23 +0530

    remove index.js

--- app.js
+++ app.js
@@ -18,7 +18,18 @@ connectToDb();
 const app = express();
 
 // Middlewares
-app.use(cors());
+// app.use(cors({
+//   origin: "https://your-frontend-url.vercel.app",
+//   credentials: true
+// }));
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://your-frontend-url.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public/uploads'));
